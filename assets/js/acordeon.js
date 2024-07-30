da0bde390d2013645161a81859829167012df55f
LuanMedeeiros <luan.medeeiros25@gmail.com>
2024-07-29 22:34:32 -0300

    Update commit

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,15 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const acordeons = document.querySelectorAll('.acordeon');
 
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
+    acordeons.forEach(acordeon => {
+        const trigger = acordeon.querySelector('.trigger');
 
-acordeonTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains('open')
+        // Certifique-se de que a classe 'open' não está presente inicialmente
+        acordeon.classList.remove('open');
 
-        if (isOpen) {
-            acordeon.classList.remove('open')
-        } else {
-            acordeon.classList.add('open')
-        }
-    })
-})
+        // Adicione um evento de clique ao gatilho
+        trigger.addEventListener('click', function () {
+            // Alternar a classe 'open' no acordeon clicado
+            acordeon.classList.toggle('open');
+        });
+    });
+});
